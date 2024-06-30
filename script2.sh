278310d025c41f50c5e41e7fb461e85cef996aef
Carlo Dandan <carlodandan.personal@proton.me>
2024-06-30 14:21:40 +0800

    patch: script2: Added new patch for s24 and M13.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- script2.sh
+++ script2.sh
@@ -18,6 +18,8 @@ cd unpack
 ../magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
 ../magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
 ../magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
+../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
+../magiskboot hexpatch system/bin/recovery 2001597ae0000054 2001597ae1000054  # ccmp w9, w25, #0, eq ; b.e #0x20 ===> b.ne #0x20
 ../magiskboot cpio vendor_ramdisk_recovery.cpio 'add 0755 system/bin/recovery system/bin/recovery'
 ../magiskboot repack ../vendor_boot.img new-boot.img
 cp new-boot.img ../vendor_boot-patched.img
